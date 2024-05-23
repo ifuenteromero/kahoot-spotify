46fed116a6c5654cabad1ba1a2e2440b9a345aee
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-05-23 20:08:07 +0200

    Set up base URL on different environments

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: `/kahoot-spotify`,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [react()],
+        base: env.VITE_BASE_URL,
+    };
 });
