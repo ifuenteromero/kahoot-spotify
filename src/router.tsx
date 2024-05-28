0522a1728cd0e67eb26e288f8279be06df775c22
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-05-28 21:16:48 +0200

    Router error page and create tests for authentication

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,30 @@
 import { createBrowserRouter } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
+import ErrorPage from './pages/ErrorPage';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import routes from './utils/routes';
 
-const router = createBrowserRouter(
-    [
-        {
-            path: routes.root,
-            element: <ProtectedRoute />,
-            children: [
-                {
-                    index: true,
-                    element: <LandingPage />,
-                },
-            ],
-        },
-        {
-            path: routes.login,
-            element: <LoginPage />,
-        },
-    ],
-    { basename: routes.baseUrl }
-);
+export const routeObjectList = [
+    {
+        path: routes.root,
+        errorElement: <ErrorPage />,
+        element: <ProtectedRoute />,
+        children: [
+            {
+                index: true,
+                element: <LandingPage />,
+            },
+        ],
+    },
+    {
+        path: routes.login,
+        element: <LoginPage />,
+    },
+];
+
+const router = createBrowserRouter(routeObjectList, {
+    basename: routes.baseUrl,
+});
 
 export default router;
--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -3,3 +3,28 @@ describe('group', () => {
         expect(1).toBeTruthy();
     });
 });
+
+// describe('App', () => {
+//     const renderComponent = () => {
+//         render(<App />);
+//         const user = userEvent.setup();
+//         return {
+//             user,
+//         };
+//     };
+//     it('should show a title ', () => {
+//         renderComponent();
+//         const title = screen.getByRole('heading');
+//         expect(title).toBeInTheDocument();
+//         expect(title.textContent).toBe(projectName);
+//     });
+
+//     // it('should toggle on-off when button is clicked', async () => {
+//     //     const { user } = renderComponent();
+//     //     const button = screen.getByRole('button');
+//     //     expect(button.textContent).toBe('on');
+//     //     await user.click(button);
+//     //     screen.debug();
+//     //     expect(button.textContent).toBe('off');
+//     // });
+// });
