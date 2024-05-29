2d55ffc310105a28e8f686904972f48ab4ac3afe
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-05-29 17:32:41 +0200

    Instal React query - npm i @tanstack/react-query - npm i -D @tanstack/react-query-devtools - set up react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
@@ -5,10 +7,15 @@ import { LoginProvider } from './contexts/LoginContext';
 import router from './router';
 import './styles/index.scss';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <LoginProvider>
-            <RouterProvider router={router} />
-        </LoginProvider>
+        <QueryClientProvider client={queryClient}>
+            <LoginProvider>
+                <RouterProvider router={router} />
+                <ReactQueryDevtools />
+            </LoginProvider>
+        </QueryClientProvider>
     </React.StrictMode>
 );
