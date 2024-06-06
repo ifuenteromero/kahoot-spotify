9c9e5a550f975c1564d19b08c677984c6fc57096
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-06-06 20:08:02 +0200

    Set up Mui Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+import { ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { theme } from './components/MUI/theme';
 import { LoginProvider } from './contexts/LoginContext';
 import router from './router';
 import './styles/index.scss';
@@ -11,11 +13,13 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <LoginProvider>
-                <RouterProvider router={router} />
-                <ReactQueryDevtools />
-            </LoginProvider>
-        </QueryClientProvider>
+        <ThemeProvider theme={theme}>
+            <QueryClientProvider client={queryClient}>
+                <LoginProvider>
+                    <RouterProvider router={router} />
+                    <ReactQueryDevtools />
+                </LoginProvider>
+            </QueryClientProvider>
+        </ThemeProvider>
     </React.StrictMode>
 );
